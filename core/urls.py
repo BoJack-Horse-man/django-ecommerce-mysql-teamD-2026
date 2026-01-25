99cccf0ebb8ab48d800b5f0c64dd566e6f20873f
Arman <armanmahfuj@gmail.com>
2026-01-25 17:03:44 +0800

    Product detail page implemented + links from list

--- core/urls.py
+++ core/urls.py
@@ -1,24 +1,9 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from shop import views  # make sure this import is present
+from shop import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),          # ← Add this line!
-    path('', views.product_list, name='product_list'),  # your product catalog """
+    path('admin/', admin.site.urls),
+    path('', views.product_list, name='product_list'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),  # this line must exist
 ]
